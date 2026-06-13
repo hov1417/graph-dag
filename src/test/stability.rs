cd9a1d7c1e7622b8b93ff09cb52a76b8893c63cd
hov1417 <hovhannes1417@gmail.com>
2026-06-13 18:05:29 +0400

    tests: more

--- src/test/stability.rs
+++ src/test/stability.rs
@@ -1,7 +1,9 @@
-use std::collections::HashMap;
 use crate::dag_to_text;
 use petgraph::Graph;
 use petgraph::acyclic::Acyclic;
+use petgraph::data::Build;
+use petgraph::graph::NodeIndex;
+use std::collections::HashMap;
 use std::panic;
 
 #[test]
@@ -15,29 +17,46 @@ fn dag_50_50() {
 
         let _ = panic::take_hook();
         let err = format!("failed convert dag to text for following graph\n'{dag:?}'");
-        panic::set_hook(Box::new(move |_| println!("{err}")));
+        panic::set_hook(Box::new(move |e| println!("{err}\n{e:?}")));
         dag_to_text(&dag);
     }
 }
 
-// TODO add remove nodes, before and after `Acyclic::try_from_graph`
 fn create_random_dag(max_vertex: u32, max_edge: u32) -> Acyclic<Graph<String, ()>> {
     let mut g = petgraph::graph::DiGraph::<String, ()>::default();
     let mut nodes = HashMap::new();
     let vert_num = (rand::random::<u32>() % max_vertex) + 1;
     let edge_num = (rand::random::<u32>() % max_edge) + 1;
-    for _ in 0..edge_num {
-        let mut a = rand::random::<u32>() % vert_num;
-        let mut b = rand::random::<u32>() % vert_num;
-        if a > b {
-            std::mem::swap(&mut a, &mut b);
-        } else if a == b {
+    for _ in 0..(edge_num / 2) {
+        if add_random_edge(&mut g, &mut nodes, (vert_num / 2) + 1) {
             continue;
         }
-        let a_node = *nodes.entry(a).or_insert_with(|| g.add_node(a.to_string()));
-        let b_node = *nodes.entry(b).or_insert_with(|| g.add_node(b.to_string()));
-        g.add_edge(a_node, b_node, ());
     }
 
-    Acyclic::try_from_graph(g).unwrap()
+    let mut g = Acyclic::try_from_graph(g).unwrap();
+
+    for _ in (edge_num / 2)..edge_num {
+        if add_random_edge(&mut g, &mut nodes, vert_num) {
+            continue;
+        }
+    }
+
+    g
+}
+
+fn add_random_edge<G>(g: &mut G, nodes: &mut HashMap<u32, NodeIndex>, vert_num: u32) -> bool
+where
+    G: Build<NodeWeight = String, EdgeWeight = (), NodeId = NodeIndex>,
+{
+    let mut a = rand::random::<u32>() % vert_num;
+    let mut b = rand::random::<u32>() % vert_num;
+    if a > b {
+        std::mem::swap(&mut a, &mut b);
+    } else if a == b {
+        return true;
+    }
+    let a_node = *nodes.entry(a).or_insert_with(|| g.add_node(a.to_string()));
+    let b_node = *nodes.entry(b).or_insert_with(|| g.add_node(b.to_string()));
+    g.add_edge(a_node, b_node, ());
+    false
 }
